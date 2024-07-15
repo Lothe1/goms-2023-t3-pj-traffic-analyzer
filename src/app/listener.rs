4f2c342383e194344c141d98882b6e9608633003
lingg <kanladaporn.sir@gmail.com>
2024-07-15 15:36:21 +0700

    Update the listener to classify each flow as incoming or outgoing based on the comparison of IPs

--- src/app/listener.rs
+++ src/app/listener.rs
@@ -2,15 +2,18 @@
 
 use clap::Parser;
 
+use netflow_parser::static_versions::v5::FlowSet;
 use netflow_parser::{NetflowParser, NetflowPacketResult};
 use ta::kafka::producer;
 use ta::cmd::listener::Args;
-use std::net::UdpSocket;
+use std::net::{UdpSocket,IpAddr};
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use rdkafka::ClientConfig;
 use rdkafka::util::Timeout;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
-use std::net::{IpAddr};
+use ta::db::ip_lookup::IPtype;
+use serde_json::{json, Value};
+use chrono::Utc;
 use tokio::net::lookup_host;
 
 
@@ -31,8 +34,8 @@ async fn main() -> std::io::Result<()> {
             let mut buf = [0; BUF_SIZE];
             let (amt, src) = socket.recv_from(&mut buf)?;
             let buf: &[u8] = &buf;
-            // classify_and_produce(&producer, buf, &listener_ip).await;
-            producer::produce_listener_to_enricher(&producer, buf).await;
+            classify_and_produce(&producer, buf, &listener_ip).await;
+            // producer::produce_listener_to_enricher(&producer, buf).await;
         }
     }
     Ok(())
@@ -41,4 +44,43 @@ async fn main() -> std::io::Result<()> {
 async fn get_listener_ip() -> Option<IpAddr> {
     let hostnames = lookup_host("localhost").await.ok()?;
     hostnames.map(|x| x.ip()).next()
+}
+
+async fn classify_and_produce(producer: &FutureProducer, buf: &[u8], listener_ip: &IpAddr) {
+    if let Some(NetflowPacketResult::V5(packet)) = NetflowParser::default().parse_bytes(buf).first() {
+        for flow in &packet.flowsets {
+            let src_ip = flow.src_addr.to_string();
+            let dst_ip = flow.dst_addr.to_string();
+
+            let packet_type = if &flow.src_addr == listener_ip {
+                IPtype::Outgoing
+            } else if &flow.dst_addr == listener_ip {
+                IPtype::Incoming
+            } else {
+                continue; // Skip packets that are neither incoming nor outgoing
+            };
+
+            let enriched_data = enrich_flow_data(flow, &src_ip, &dst_ip, packet_type);
+            let buf = serde_json::to_vec(&enriched_data).unwrap();
+            producer::produce_listener_to_enricher(producer, &buf).await;
+        }
+    }
+}
+
+fn enrich_flow_data(flow: &FlowSet, src_ip: &str, dst_ip: &str, packet_type: IPtype) -> Value {
+    json!({
+        "measurement": "netflow",
+        "tags": {
+            "src_ip": src_ip,
+            "dst_ip": dst_ip,
+            "type": format!("{:?}", packet_type)
+        },
+        "fields": {
+            "packets": flow.d_pkts,
+            "bytes": flow.d_octets,
+            "first_switched": flow.first,
+            "last_switched": flow.last
+        },
+        "time": Utc::now()
+    })
 }
