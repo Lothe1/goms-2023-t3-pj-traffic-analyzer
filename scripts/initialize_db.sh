5f816510b23326446e66517cbc60be8c770683e7
Tath Kanchanarin <tath9982@gmail.com>
2024-07-15 19:43:19 +0700

    make entrypoint and dockerfile so that initialze_db.sh runs automatically

--- scripts/initialize_db.sh
+++ scripts/initialize_db.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+wait_for_influxdb() {
+    until curl -s -I -XGET http://influxdb:8086/ping > /dev/null; do
+        echo "InfluxDB is not yet ready - waiting..."
+        sleep 2
+    done
+}
+
+
+wait_for_influxdb
+
 influx config rm default
 # Setup InfluxDB
 influx setup \
@@ -8,4 +19,5 @@ influx setup \
   --org doglver \
   --bucket db \
   --retention 24h\
-  --force
+  --force \
+  --host http://influxdb:8086 
