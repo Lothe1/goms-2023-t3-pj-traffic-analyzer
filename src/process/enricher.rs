a2a2101c08ce88c474b96082de217818ea476ca2
lingg <kanladaporn.sir@gmail.com>
2024-07-18 22:31:25 +0700

    Support v9

--- src/process/enricher.rs
+++ src/process/enricher.rs
@@ -2,6 +2,7 @@ use chrono::Utc;
 use netflow_parser::static_versions::v5::{FlowSet, Header};
 use netflow_parser::variable_versions::common::FieldValue;
 use netflow_parser::variable_versions::ipfix_lookup::IPFixField;
+use netflow_parser::variable_versions::v9_lookup::V9Field;
 use netflow_parser::{NetflowPacketResult, NetflowParser};
 use netflow_parser::static_versions::v5::FlowSet as FlowSetV5;
 use netflow_parser::variable_versions::v9::{FlowSet as FlowSetV9, TemplateField as TemplateFieldV9};
@@ -28,7 +29,8 @@ pub async fn enrich_packet(payload: Vec<u8>, cidr_lookup: CidrLookup) -> Vec<Vec
             NetflowPacketResult::V9(packet) => {
                 println!("Parsing NetFlow v9 with {} flows", packet.flowsets.len());
                 for flow in &packet.flowsets {
-                    enrich_flow_v9(flow, &flow.body.templates, &cidr_lookup, &mut enriched_packets);
+                    // enrich_flow_v9(flow, &flow.body.templates, &cidr_lookup, &mut enriched_packets);
+                    enrich_flow_v9(flow, &cidr_lookup, &mut enriched_packets);
                 }
             },
             NetflowPacketResult::IPFix(packet) => {
@@ -47,6 +49,8 @@ pub async fn enrich_packet(payload: Vec<u8>, cidr_lookup: CidrLookup) -> Vec<Vec
     enriched_packets
 }
 
+
+// NetFlow v5
 fn enrich_flow_v5(flow: &netflow_parser::static_versions::v5::FlowSet, 
                   cidr_lookup: &CidrLookup, 
                   enriched_packets: &mut Vec<Vec<u8>>) {
@@ -95,66 +99,101 @@ fn enrich_flow_v5(flow: &netflow_parser::static_versions::v5::FlowSet,
 }
 
 
+// NetFlow v9
 fn enrich_flow_v9(flow: &netflow_parser::variable_versions::v9::FlowSet, 
-                  template: &std::option::Option<std::vec::Vec<netflow_parser::variable_versions::v9::Template>>, 
                   cidr_lookup: &CidrLookup, 
                   enriched_packets: &mut Vec<Vec<u8>>) {
 
-    // if let Some(temp) = template {
-    //     println!("Template: {:?}", temp);
-    //     let mut template_map: HashMap<u16, Vec<TemplateFieldV9>> = HashMap::new();
-        
-    //     // Build a map for quick lookup
-    //     for t in temp {
-    //         template_map.insert(t.template_id, t.fields.clone());
-    //     }
-        
-        
-    //     let src_ip = template_map.get(&8).map(|v| v.clone()).unwrap_or_else(|| "Unknown".to_string());
-    //     let dst_ip = template_map.get(&12).unwrap();
-    //     let src_country = cidr_lookup.lookup_country(&src_ip).unwrap_or(&"Unknown".to_string()).clone();
-    //     let dst_country = cidr_lookup.lookup_country(&dst_ip).unwrap_or(&"Unknown".to_string()).clone();
-    //     let (src_asn, src_as_name) = cidr_lookup.lookup_as(&src_ip).unwrap_or(&(String::from("Unknown"), String::from("Unknown"))).clone();
-    //     let (dst_asn, dst_as_name) = cidr_lookup.lookup_as(&dst_ip).unwrap_or(&(String::from("Unknown"), String::from("Unknown"))).clone();
+    if let Some(f) = &flow.body.data {
+        for data_record in &f.data_fields {
+            let mut src_ip = String::new();
+            let mut dst_ip = String::new();
+            let mut packets = 0;
+            let mut bytes = 0;
+            let mut first_switched = 0;
+            let mut last_switched = 0;
 
-    //     let packet_type = match (is_private_ip(&src_ip), is_private_ip(&dst_ip)) {
-    //         (true, true) => Some(IPtype::Incoming),
-    //         (true, _) => Some(IPtype::Outgoing),
-    //         (_, true) => Some(IPtype::Incoming),
-    //         (_, _) => Some(IPtype::Outgoing)
-    //     };
+            for (_, (_, (field_type, field_value))) in data_record.iter().enumerate() {
+                match field_type {
+                    V9Field::Ipv4SrcAddr => {
+                        src_ip = extract_ip_address(&field_value).unwrap_or("Unknown".to_string());
+                    },
+                    V9Field::Ipv4DstAddr => {
+                        dst_ip = extract_ip_address(&field_value).unwrap_or("Unknown".to_string());
+                    },
+                    V9Field::InPkts => {
+                        packets = match field_value {
+                            FieldValue::Float64(val) => *val as u64,
+                            _ => 0
+                        };
+                    },
+                    V9Field::InBytes => {
+                        bytes = match field_value {
+                            FieldValue::Float64(val) => *val as u32,
+                            _ => 0
+                        };
+                    }, 
+                    V9Field::FirstSwitched => {
+                        first_switched = match field_value {
+                            FieldValue::Float64(val) => *val as u64,
+                            _ => 0
+                        };
+                    },
+                    V9Field::LastSwitched => {
+                        last_switched = match field_value {
+                            FieldValue::Float64(val) => *val as u64,
+                            _ => 0
+                        };
+                    },
+                    _ => { }
+                }
+            }
 
-    //     if packet_type.is_some() {
-    //         let time = Utc::now();
-    //         let enriched_data = json!({
-    //             "measurement": "netflow",
-    //             "tags": {
-    //                 "src_ip": src_ip.clone(),
-    //                 "dst_ip": dst_ip.clone(),
-    //                 "src_country": src_country.clone(),
-    //                 "dst_country": dst_country.clone(),
-    //                 "src_asn": src_asn.clone(),
-    //                 "src_as_name": src_as_name.clone(),
-    //                 "dst_asn": dst_asn.clone(),
-    //                 "dst_as_name": dst_as_name.clone(),
-    //                 "type": format!("{:?}", packet_type.unwrap()).clone()
-    //             },
-    //             "fields": {
-    //                 "packets": template_map.get(&2).unwrap(),
-    //                 "bytes": template_map.get(&1).unwrap(),
-    //                 "first_switched": template_map.get(&4).unwrap(),
-    //                 "last_switched": template_map.get(&7).unwrap()
-    //             },
-    //             "time": time
-    //         });
-    //         println!("{:?}", enriched_data);
-    //         let buf = serde_json::to_vec(&enriched_data).unwrap();
-    //         enriched_packets.push(buf); 
-    //     }
-    // }
+            let src_country = cidr_lookup.lookup_country(&src_ip).unwrap_or(&"Unknown".to_string()).clone();
+            let dst_country = cidr_lookup.lookup_country(&dst_ip).unwrap_or(&"Unknown".to_string()).clone();
+            let (src_asn, src_as_name) = cidr_lookup.lookup_as(&src_ip).unwrap_or(&(String::from("Unknown"), String::from("Unknown"))).clone();
+            let (dst_asn, dst_as_name) = cidr_lookup.lookup_as(&dst_ip).unwrap_or(&(String::from("Unknown"), String::from("Unknown"))).clone();
+
+            let packet_type = match (is_private_ip(&src_ip), is_private_ip(&dst_ip)) {
+                (true, true) => Some(IPtype::Incoming),
+                (true, _) => Some(IPtype::Outgoing),
+                (_, true) => Some(IPtype::Incoming),
+                (_, _) => Some(IPtype::Outgoing)
+            };
+
+            if packet_type.is_some() {
+                let time = Utc::now();
+                let enriched_data = json!({
+                    "measurement": "netflow",
+                    "tags": {
+                        "src_ip": src_ip.clone(),
+                        "dst_ip": dst_ip.clone(),
+                        "src_country": src_country.clone(),
+                        "dst_country": dst_country.clone(),
+                        "src_asn": src_asn.clone(),
+                        "src_as_name": src_as_name.clone(),
+                        "dst_asn": dst_asn.clone(),
+                        "dst_as_name": dst_as_name.clone(),
+                        "type": format!("{:?}", packet_type.unwrap()).clone()
+                    },
+                    "fields": {
+                        "packets": packets,
+                        "bytes": bytes,
+                        "first_switched": first_switched,
+                        "last_switched": last_switched
+                    },
+                    "time": time
+                });
+                println!("{:?}", enriched_data);
+                let buf = serde_json::to_vec(&enriched_data).unwrap();
+                enriched_packets.push(buf);
+            }
+        }
+    }
 }
 
 
+// Extract IP address from field value
 fn extract_ip_address(field_val: &FieldValue) -> Option<String> {
     match field_val {
         FieldValue::Ip4Addr(ip) => Some(ip.to_string()),
@@ -163,7 +202,10 @@ fn extract_ip_address(field_val: &FieldValue) -> Option<String> {
 }
 
 
-fn enrich_flow_ipfix(flow: &netflow_parser::variable_versions::ipfix::FlowSet, cidr_lookup: &CidrLookup, enriched_packets: &mut Vec<Vec<u8>>) {
+// IPFIX
+fn enrich_flow_ipfix(flow: &netflow_parser::variable_versions::ipfix::FlowSet, 
+                     cidr_lookup: &CidrLookup, 
+                     enriched_packets: &mut Vec<Vec<u8>>) {
     if let Some(f) = &flow.body.data {
         for data_record in &f.data_fields {
             let mut src_ip = String::new();
@@ -248,7 +290,6 @@ fn enrich_flow_ipfix(flow: &netflow_parser::variable_versions::ipfix::FlowSet, c
                 let buf = serde_json::to_vec(&enriched_data).unwrap();
                 enriched_packets.push(buf);
             }
-
         }
     }
 }
